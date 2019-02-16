506ffeac2e6e8f290ebdc606f7c222fac8fcad1f
Jared Feeley <jf3925@rit.edu>
2019-02-16 17:48:39 -0500

    Require request function

--- index.js
+++ index.js
@@ -4,6 +4,7 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const request = require('request');
 const mysql = require('mysql');
 require('dotenv').load();
 
@@ -39,6 +40,5 @@ request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
   currBod = body.results;
   console.log(currBod);
 });
-
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
