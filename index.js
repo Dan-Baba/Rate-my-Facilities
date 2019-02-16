cf589004dac9e59a2d549bfe12c7871f70fcca73
Kevin Barkevich <kdb2713@rit.edu>
2019-02-16 16:47:42 -0500

    Added request to google maps/cloud servers to fetch locations near RIT.

--- index.js
+++ index.js
@@ -5,10 +5,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const request = require('request');
 require('dotenv').load();
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
+
+let currBod;
+
+request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
+ + process.env.MY_API_KEY + '&location=43.084589,-77.674344&rankby=distance',
+{json: true}, (err, res, body) => {
+  if (err) {
+    return console.log(err);
+  }
+  console.log(body.url);
+  console.log(body.explanation);
+  currBod = body.results;
+  console.log(currBod);
+});
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
