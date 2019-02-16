63d45ea8021bc093e85769fa9d8e989f00aa0d1a
Jared Feeley <jf3925@rit.edu>
2019-02-16 16:19:54 -0500

    Add temporary SQL connection test

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const mysql = require('mysql');
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+connection = mysql.createConnection({
+  host: '34.73.250.171',
+  user: 'root',
+  password: '',
+  database: 'rmf',
 });
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+connection.connect();
+connection.query('SELECT * FROM facilities', function(error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results[0].placeID);
+});
+connection.end();
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
