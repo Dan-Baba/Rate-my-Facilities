4978061612f9144033853053408e0f382d90dea7
Kevin Barkevich <kdb2713@rit.edu>
2019-02-16 18:27:21 -0500

    asasas

--- index.js
+++ index.js
@@ -20,26 +20,33 @@ connection.end();
 const express = require('express');
 const app = express();
 const port = 3000;
-const request = require('request');
+const fetch = require('node-fetch');
 require('dotenv').load();
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
+app.use(require('./routes/facilities'));
 
-let currBod;
 
-request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
- + process.env.MY_API_KEY + '&location=43.084589,-77.674344&rankby=distance',
-{json: true}, (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(body.url);
-  console.log(body.explanation);
-  currBod = body.results;
-  console.log(currBod);
-});
+const asyncFunction = async () => {
+  const currBod = await fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
+  + process.env.MY_API_KEY + '&location=43.084589,-77.674344&rankby=distance');
+  const response = await currBod.json();
+  // await http.get('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
+  // + process.env.MY_API_KEY + '&location=43.084589,-77.674344&rankby=distance',
+  // {json: true}, (err, res, body) => {
+  //   if (err) {
+  //     return console.log(err);
+  //   }
+  //   currBod = body;
+  // });
+  // console.log(response);
+  return response;
+};
 
+/*
+ * hasBod
+ */
+function hasBod(bod) {
+  console.log(bod);
+}
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
