8b39ad3fe67c9d67d112b783e9a7c77218ec6820
Jared Feeley <jf3925@rit.edu>
2019-02-16 16:58:51 -0500

    Change database test

--- index.js
+++ index.js
@@ -1,29 +1,29 @@
 /*
  * index.js
  */
+const express = require('express');
+const app = express();
+const port = 3000;
 const mysql = require('mysql');
 
 connection = mysql.createConnection({
   host: '34.73.250.171',
   user: 'root',
-  password: '',
+  password: 'db_test',
   database: 'rmf',
 });
 
 connection.connect();
+result = 0;
 connection.query('SELECT * FROM facilities', function(error, results, fields) {
   if (error) throw error;
+  result = results[0];
   console.log('The solution is: ', results[0].placeID);
 });
 connection.end();
 
-const express = require('express');
-const app = express();
-const port = 3000;
-require('dotenv').load();
-
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.send('' + result.placeID);
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
