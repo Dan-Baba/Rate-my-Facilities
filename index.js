7cff519b6cd1f00527497c87e2422b8a5590e3b7
Dan Foote <danfoote@Dans-MacBook-Pro.local>
2019-02-16 19:49:01 -0500

    Fixed our Kevin problem

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
-const request = require('request');
+
 const mysql = require('mysql');
 require('dotenv').load();
 
@@ -41,19 +41,6 @@ const asyncFunction = async () => {
   // console.log(response);
   return response;
 };
-
-
-request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
- + process.env.MY_API_KEY + '&location=43.084589,-77.674344&rankby=distance',
-{json: true}, (err, res, body) => {
-  if (err) {
-    return console.log(err);
-  }
-  console.log(body.url);
-  console.log(body.explanation);
-  currBod = body.results;
-  console.log(currBod);
-});
 
 app.use(bodyParser.json());
 app.use(require('./routes/auth'));
--- routes/facilities.js
+++ routes/facilities.js
@@ -1,7 +1,18 @@
 const router = require('express').Router();
+const fetch = require('node-fetch');
 
-router.get('/', (req, res) => {
-  res.send('Hello World!');
+// Functions
+const asyncFunction = async (req, res, next) => {
+  const currBod = await fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
+  + process.env.MY_API_KEY + '&location=43.084589,-77.674344&rankby=distance');
+  const response = await currBod.json();
+  req.data = response.results;
+  next();
+};
+
+// Routes
+router.get('/', asyncFunction, (req, res) => {
+  res.send(req.data);
 });
 
 module.exports = router;
