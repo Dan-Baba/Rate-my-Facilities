051aad2b0b3dab75ca83d3f485b8c7198526f744
Jared Feeley <jf3925@rit.edu>
2019-02-16 19:11:05 -0500

    Sync up

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 /*
  * index.js
  */
+const bodyParser = require('body-parser');
 const express = require('express');
 const app = express();
 const port = 3000;
@@ -42,19 +43,14 @@ const asyncFunction = async () => {
   return response;
 };
 
-/*
+/**
  * hasBod
+ * @param bod the body
  */
 function hasBod(bod) {
   console.log(bod);
 }
 
-=======
-app.get('/', (req, res) => {
-  res.send('' + result.placeID);
-});
-
->>>>>>> f138026cef228b28221d0ba49d7024693ab698ce
 request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
  + process.env.MY_API_KEY + '&location=43.084589,-77.674344&rankby=distance',
 {json: true}, (err, res, body) => {
@@ -67,11 +63,8 @@ request('https://maps.googleapis.com/maps/api/place/nearbysearch/json?key='
   console.log(currBod);
 });
 
-<<<<<<< HEAD
+app.use(bodyParser.json());
 app.use(require('./routes/auth'));
 app.use(require('./routes/facilities'));
 
-=======
->>>>>>> 506ffeac2e6e8f290ebdc606f7c222fac8fcad1f
->>>>>>> f138026cef228b28221d0ba49d7024693ab698ce
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
+
 router.post('/', (req, res) => {
   console.log(req.body);
+  res.send('POST success');
 });
 
 module.exports = router;
